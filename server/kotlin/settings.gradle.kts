f447716b2627338bcd08fcbf7c4c091c0b58a6ef
San Kim <waterfog9580@gmail.com>
2024-07-05 22:28:25 +0900

    ✨ Chat Service 관련 패키지 구조 설정

--- server/kotlin/settings.gradle.kts
+++ server/kotlin/settings.gradle.kts
@@ -16,3 +16,16 @@ project(":user:infrastructure:jpa").projectDir = file("user/infrastructure/jpa")
 
 include(":user:bootstrap:api")
 project(":user:bootstrap:api").projectDir = file("user/bootstrap/api")
+
+// chat-service
+include(":chat:domain")
+project(":chat:domain").projectDir = file("chat/domain")
+
+include(":chat:application")
+project(":chat:application").projectDir = file("chat/application")
+
+include(":chat:infrastructure:kafka")
+project(":chat:infrastructure:kafka").projectDir = file("chat/infrastructure/kafka")
+
+include(":chat:bootstrap:api")
+project(":chat:bootstrap:api").projectDir = file("chat/bootstrap/api")
